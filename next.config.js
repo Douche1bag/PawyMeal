c5181c6964376365f00d121a1cf96870cbd491da
Douche1bag <Ksouythong8@gmail.com>
2025-10-05 16:37:07 +0700

    delconfig.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['drive.google.com', 'lh3.googleusercontent.com'],
-    unoptimized: true,
-  },
-}
-
-module.exports = nextConfig 
